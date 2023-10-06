cd176e04124166482669b56b1285221a2b941ec9
rubayet211 <rubayet211@gmail.com>
2023-10-06 08:59:13 +0600

    u1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService){}
+export class AuthController {
+    constructor(private authService: AuthService) { }
 
     @Post('signup')
     signup() {
-        return 'I am signup';
+        return { msg: 'I am signup' };
     }
 
     @Post('signin')
