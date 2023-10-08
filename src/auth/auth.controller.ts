927fcd7bb49297c386cfbe779438745b01a22523
rubayet211 <rubayet211@gmail.com>
2023-10-09 00:02:57 +0600

    5th commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('signup')
-    signup() {
-        return this.authService.signup();
+    @UsePipes(new ValidationPipe())
+    signup(@Body() authdto: AuthDto) {
+        return { data: authdto.email };
     }
 
     @Post('signin')
+    @UsePipes(new ValidationPipe())
     signin() {
         return this.authService.signin();
     }
+
 }
